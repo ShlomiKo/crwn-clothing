64a50121a7f46f4585431f69022c881ad1828b2b
Shlomi Koren <koren.shlomi1@gmail.com>
2021-11-13 11:44:57 +0200

    added a with-spinner HOC

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -18,7 +18,8 @@ export const selectCollections = createSelector(
 
 export const selectCollectionsForPreview = createSelector(
   [selectCollections],
-  (collections) => Object.keys(collections).map((key) => collections[key])
+  (collections) =>
+    collections ? Object.keys(collections).map((key) => collections[key]) : []
 );
 
 // export const selectCollection = memoize((collectionUrlParam) =>
@@ -30,8 +31,7 @@ export const selectCollectionsForPreview = createSelector(
 // );
 
 export const selectCollection = memoize((collectionUrlParam) =>
-  createSelector(
-    [selectCollections],
-    (collections) => collections[collectionUrlParam]
+  createSelector([selectCollections], (collections) =>
+    collections ? collections[collectionUrlParam] : null
   )
 );
